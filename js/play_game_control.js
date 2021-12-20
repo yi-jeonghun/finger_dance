4da998849cb5a5f03c1c2105de3048411480bd56
Jeonghun Lee <yi.jeonghun@gmail.com>
2021-12-20 20:34:31 +0900

    close iab

--- js/play_game_control.js
+++ js/play_game_control.js
@@ -134,8 +134,8 @@ function PlayGameControl(){
 		// $('#id_btn_back').on('click', function(){ window.history.back(); });
 		$('#id_btn_back').on('click', function(){ 
 			console.log('button back ');
-			// window.close(); 
-			window.location = 'close_iab';
+			// window.close();
+			localStorage.setItem('close_iab', '1');
 		});
 		$('#id_btn_home').on('click', function(){ document.location.href = 'https://beatmaster.me'; });
 		$('#id_btn_rank').on('click', self.OnRankBtnClick);
