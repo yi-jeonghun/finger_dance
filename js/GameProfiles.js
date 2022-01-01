e166de8dea7f54eb3415996606ee0e0aa769806f
Jeonghun Lee <yi.jeonghun@gmail.com>
2022-01-01 21:50:32 +0900

    game profile 개념 도입.

---
+++ js/GameProfiles.js
@@ -0,0 +1,78 @@
+class GameProfiles{
+	#ctx;
+	#width;
+	#height;
+	#base_line;
+
+	constructor(ctx, width, height, base_line){
+		this.#ctx = ctx;
+		this.#width = width;
+		this.#height = height;
+		this.#base_line = base_line;
+	}
+
+	LoadGameProfile(){
+		var quarter_x = this.#width / 4;
+		var first_x = quarter_x / 2;
+
+		{//guide lines
+			var line_width = 1;
+			var life_ms = -1;
+			var base_line_draw_obj = new DrawLine(this.#ctx, 0, this.#base_line, this.#width, this.#base_line, line_width, 'RED', life_ms);
+
+			var vertical_line1 = new DrawLine(this.#ctx, quarter_x, 0, quarter_x, this.#height, line_width, '#aaa', life_ms);
+			var vertical_line2 = new DrawLine(this.#ctx, quarter_x*2, 0, quarter_x*2, this.#height, line_width, '#aaa', life_ms);
+			var vertical_line3 = new DrawLine(this.#ctx, quarter_x*3, 0, quarter_x*3, this.#height, line_width, '#aaa', life_ms);
+
+			base_line_draw_obj.Update();
+			vertical_line1.Update();
+			vertical_line2.Update();
+			vertical_line3.Update();
+			window._renderer.AddDrawObject(1, base_line_draw_obj);
+			window._renderer.AddDrawObject(1, vertical_line1);
+			window._renderer.AddDrawObject(1, vertical_line2);
+			window._renderer.AddDrawObject(1, vertical_line3);
+		}
+
+		{//empty guide
+			var dw = 65;
+			var dh = 65;
+			
+			var di_l = new DrawImage(this.#ctx, _atlas._img, 
+				_atlas._img_l_empty.x, _atlas._img_l_empty.y, _atlas._img_l_empty.w, _atlas._img_l_empty.h, 
+				(first_x - dw/2), this.#base_line - dh/2, dw, dh, -1);
+			di_l.Update();
+			window._renderer.AddDrawObject(4, di_l);
+			
+			var di_d = new DrawImage(this.#ctx, _atlas._img, 
+				_atlas._img_d_empty.x, _atlas._img_d_empty.y, _atlas._img_d_empty.w, _atlas._img_d_empty.h, 
+				((first_x + quarter_x) - dw/2), this.#base_line - dh/2, dw, dh, -1);
+			di_d.Update();
+			window._renderer.AddDrawObject(4, di_d);
+
+			var di_u = new DrawImage(this.#ctx, _atlas._img, 
+				_atlas._img_u_empty.x, _atlas._img_u_empty.y, _atlas._img_u_empty.w, _atlas._img_u_empty.h, 
+				((first_x + quarter_x*2) - dw/2), this.#base_line - dh/2, dw, dh, -1);
+			di_u.Update();
+			window._renderer.AddDrawObject(4, di_u);
+	
+			var di_r = new DrawImage(this.#ctx, _atlas._img, 
+				_atlas._img_r_empty.x, _atlas._img_r_empty.y, _atlas._img_r_empty.w, _atlas._img_r_empty.h, 
+				((first_x + quarter_x*3) - dw/2), this.#base_line - dh/2, dw, dh, -1);
+			di_r.Update();
+			window._renderer.AddDrawObject(4, di_r);
+		}
+	}
+
+	GetProgressBar(){
+		return new DrawProgressBar(this.#ctx, 0, 0, this.#width, 0, 20, 'RED');
+	}
+
+	GetComboText(){
+		return new DrawText(this.#ctx, '', 200, 220, 25, 'blue', -1);
+	}
+
+	GetScoreText(){
+		return new DrawText(this.#ctx, '', 200, 50, 50, 'red', -1);
+	}
+}
--- js/game_control.js
+++ js/game_control.js
@@ -287,74 +287,20 @@ function GameControl(width, height){
 	this.PrepareGame = function(){
 		window._renderer.ClearDrawObject();
 
+		var profile = new GameProfiles(window._renderer._ctx, self._width, self._height, self._base_line);
 		{
-			var ctx = window._renderer._ctx;
-			var quarter_x = self._width / 4;
-			var first_x = quarter_x / 2;
-
-			{//guide lines
-				var line_width = 1;
-				var life_ms = -1;
-				var base_line_draw_obj = new DrawLine(ctx, 0, self._base_line, self._width, self._base_line, line_width, 'RED', life_ms);
-	
-				var vertical_line1 = new DrawLine(ctx, quarter_x, 0, quarter_x, self._height, line_width, '#aaa', life_ms);
-				var vertical_line2 = new DrawLine(ctx, quarter_x*2, 0, quarter_x*2, self._height, line_width, '#aaa', life_ms);
-				var vertical_line3 = new DrawLine(ctx, quarter_x*3, 0, quarter_x*3, self._height, line_width, '#aaa', life_ms);
-	
-				base_line_draw_obj.Update();
-				vertical_line1.Update();
-				vertical_line2.Update();
-				vertical_line3.Update();
-				window._renderer.AddDrawObject(1, base_line_draw_obj);
-				window._renderer.AddDrawObject(1, vertical_line1);
-				window._renderer.AddDrawObject(1, vertical_line2);
-				window._renderer.AddDrawObject(1, vertical_line3);
-			}
-			{//empty guide
-				var dw = 65;
-				var dh = 65;
-				
-				var di_l = new DrawImage(ctx, _atlas._img, 
-					_atlas._img_l_empty.x, _atlas._img_l_empty.y, _atlas._img_l_empty.w, _atlas._img_l_empty.h, 
-					(first_x - dw/2), self._base_line - dh/2, dw, dh, -1);
-				di_l.Update();
-				window._renderer.AddDrawObject(4, di_l);
-				
-				var di_d = new DrawImage(ctx, _atlas._img, 
-					_atlas._img_d_empty.x, _atlas._img_d_empty.y, _atlas._img_d_empty.w, _atlas._img_d_empty.h, 
-					((first_x + quarter_x) - dw/2), self._base_line - dh/2, dw, dh, -1);
-				di_d.Update();
-				window._renderer.AddDrawObject(4, di_d);
-
-				var di_u = new DrawImage(ctx, _atlas._img, 
-					_atlas._img_u_empty.x, _atlas._img_u_empty.y, _atlas._img_u_empty.w, _atlas._img_u_empty.h, 
-					((first_x + quarter_x*2) - dw/2), self._base_line - dh/2, dw, dh, -1);
-				di_u.Update();
-				window._renderer.AddDrawObject(4, di_u);
-		
-				var di_r = new DrawImage(ctx, _atlas._img, 
-					_atlas._img_r_empty.x, _atlas._img_r_empty.y, _atlas._img_r_empty.w, _atlas._img_r_empty.h, 
-					((first_x + quarter_x*3) - dw/2), self._base_line - dh/2, dw, dh, -1);
-				di_r.Update();
-				window._renderer.AddDrawObject(4, di_r);
-			}
+			profile.LoadGameProfile();
 			{//progress bar
-				if(self._draw_progress_bar == null){
-					self._draw_progress_bar = new DrawProgressBar(ctx, 0, 0, self._width, 0, 20, 'RED');			
-				}
+				self._draw_progress_bar = profile.GetProgressBar();
 				window._renderer.AddDrawObject(3, self._draw_progress_bar);
 			}
 			{//combo text
-				if(self._draw_text_combo == null){
-					self._draw_text_combo = new DrawText(ctx, '', 200, 220, 25, 'blue', -1);
-				}
+				self._draw_text_combo = profile.GetComboText();
 				self._draw_text_combo.Update();
 				window._renderer.AddDrawObject(7, self._draw_text_combo);
 			}
 			{//score text
-				if(self._draw_text_score == null){
-					self._draw_text_score = new DrawText(ctx, '', 200, 50, 50, 'red', -1);
-				}
+				self._draw_text_score = profile.GetScoreText();
 				self._draw_text_score.Update();
 				window._renderer.AddDrawObject(7, self._draw_text_score);
 			}
