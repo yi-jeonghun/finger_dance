1a8655138d691fe3aa9318dff949e18973dbbe58
Jeonghun Lee <yi.jeonghun@gmail.com>
2022-01-01 18:46:43 +0900

    사용하지 않는 코드 삭제

--- js/play_game_control.js
+++ js/play_game_control.js
@@ -1,5 +1,4 @@
 $('document').ready(function(){
-	window._drum_player = null;//new DrumPlayer().Init();
 	window._atlas = new Atlas().Init();
 	window._timer = new Timer().Init();
 
@@ -10,12 +9,6 @@ function PlayGameControl(){
 	var self = this;
 	this._game_id = null;
 	this._video_id = null;
-	this._sound_key_list = [
-		DEFAULT_SOUND_KEY,
-		DEFAULT_SOUND_KEY,
-		DEFAULT_SOUND_KEY,
-		DEFAULT_SOUND_KEY
-	];
 	this._i_like = false;
 	this._my_score = -1;
 	this._my_rank = -1;
@@ -34,10 +27,6 @@ function PlayGameControl(){
 		}
 		console.log('self._is_mobile_device ' + self._is_mobile_device);
 
-		_auth_control._cb_on_login = self.OnLogin;
-		_auth_control._cb_on_logout = self.OnLogOut;
-		_auth_control._cb_on_login_fail = self.OnLoginFail;
-
 		self.InitLayout();
 		self.InitComponentHandle();
 		self.InitGameModules();
@@ -55,8 +44,6 @@ function PlayGameControl(){
 				self.DisplayGameData(window._game_data);
 			});
 	
-			// self.DisplayGameData(window._game_data);
-			// self.GetLike();
 			// self.GetRanking();
 		}
 
@@ -125,10 +112,7 @@ function PlayGameControl(){
 			webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(message));
 		});
 		$('#id_btn_home').on('click', function(){ document.location.href = 'https://beatmaster.me'; });
-		$('#id_btn_rank').on('click', self.OnRankBtnClick);
-		$('#id_btn_rank_hide').on('click', self.OnRankHideClick);
 		$('#id_btn_stop_small').on('click', self.OnStopBtnClick);
-		$('#id_btn_fullscreen').on('click', self.OnFullScreenBtnClick);
 		$('#id_img_vibration').on('click', self.OnVibrationClick);
 		$('#id_btn_stop').on('click', self.OnStopBtnClick);
 		$('#id_btn_retry').on('click', self.RetryGame);
@@ -150,50 +134,6 @@ function PlayGameControl(){
 			$('#id_txt_vibration').html('OFF');
 			$('#id_img_vibration').attr('src', 'img/vibration_off.png');
 		}
-	};
-
-	this.OnFullScreenBtnClick = function(){
-		var elem = document.getElementById("lo_play");
-		if(self._is_fullscreen == true){
-			self._is_fullscreen = false;
-			$('#id_icon_fullscreen').removeClass('fa-compress');
-			$('#id_icon_fullscreen').removeClass('fa-expand');
-			$('#id_icon_fullscreen').addClass('fa-compress');
-
-		  if (document.exitFullscreen) {
-				document.exitFullscreen();
-			} else if (document.mozCancelFullScreen) { /* Firefox */
-				document.mozCancelFullScreen();
-			} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-				document.webkitExitFullscreen();
-			} else if (document.msExitFullscreen) { /* IE/Edge */
-				document.msExitFullscreen();
-			}
-		}else{
-			self._is_fullscreen = true;
-			$('#id_icon_fullscreen').removeClass('fa-compress');
-			$('#id_icon_fullscreen').removeClass('fa-expand');
-			$('#id_icon_fullscreen').addClass('fa-compress');
-
-			if (elem.requestFullscreen) {
-				elem.requestFullscreen();
-			} else if (elem.mozRequestFullScreen) { /* Firefox */
-				elem.mozRequestFullScreen();
-			} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-				elem.webkitRequestFullscreen();
-			} else if (elem.msRequestFullscreen) { /* IE/Edge */
-				elem.msRequestFullscreen();
-			}
-		}
-		setTimeout(self.InitLayout, 300);
-	};
-
-	this.OnRankBtnClick = function(){
-		self.RankingLayerShowHide(true);
-	};
-
-	this.OnRankHideClick = function(){
-		self.RankingLayerShowHide(false);
 	};
 
 	this.ShowHidePlayStopButton = function(show_play, show_stop){
@@ -236,15 +176,6 @@ function PlayGameControl(){
 		}
 	};
 
-	this.RankingLayerShowHide = function(is_show){
-		if(is_show){
-			$('#rank_layer').css('visibility', 'visible');
-			self.GetRanking();
-		}else{
-			$('#rank_layer').css('visibility', 'hidden');
-		}
-	};
-
 	this.PlayGame = function(){
 		if(self._is_mobile_device){
 			self.ChangeLayout(LAYOUT.PLAY);
@@ -275,14 +206,6 @@ function PlayGameControl(){
 		if(_game_control._is_playing){
 			self.SaveGameResult(is_complete, progress_percent, score);
 			if(is_complete == true){
-				if(_auth_control._user_info == null){
-					// self.ShowHideLogin(true);
-					// self._need_to_save_score = true;
-				}else{
-					if(self._my_score < _game_control._score){
-						self.SaveScore();
-					}
-				}
 			}else{
 				setTimeout(self.ShowRetryButton, 500);
 				window._game_control.StopGame();
@@ -325,8 +248,6 @@ function PlayGameControl(){
 	this.DisplayGameData = function(game_data){
 		self._video_id = game_data.video_id;
 		console.log('self._video_id ' + self._video_id);
-		self._sound_key_list = JSON.parse(game_data.sound_key_list);
-		// self.LoadInitDrums();
 
 		$('#id_label_title').html(game_data.title);
 		$('#id_label_artist').html(game_data.artist);
@@ -355,38 +276,7 @@ function PlayGameControl(){
 		self.ShowHidePlayStopButton(true, false);
 	};
 
-	this.LoadInitDrums = function(){
-		_drum_player.LoadDrum(0, self._sound_key_list[0]);
-		_drum_player.LoadDrum(1, self._sound_key_list[1]);
-		_drum_player.LoadDrum(2, self._sound_key_list[2]);
-		_drum_player.LoadDrum(3, self._sound_key_list[3]);
-	};
-
-	this.GetLike = function(){
-		var data = {
-			game_id:self._game_id
-		};
-		$.ajax({
-			url: '/beat_api/get_like',
-			type: 'POST',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			success: function (res) {
-				if(res.ok){
-					self.UpdateLikeButton(res.like_data);
-				}else{
-					alert(res.err);
-				}
-			}
-		});
-	};
-
 	this.Like = function(){
-		if(_auth_control._user_info == null){
-			alert('login');
-			return;
-		}
 		var data = {
 			game_id: self._game_id,
 			like: (self._i_like == true ? false : true)
@@ -423,106 +313,6 @@ function PlayGameControl(){
 		}
 	};
 
-	this.GetMyScoreAndRank = function(first){
-		var data = {
-			game_id:self._game_id
-		};
-		$.ajax({
-			url: '/beat_api/get_my_score_rank',
-			type: 'POST',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			success: function (res) {
-				if(res.ok){
-					if(first == false){
-						console.log('self._my_score ' + self._my_score);
-						console.log('res.data.score ' + res.data.score);
-						if(self._my_score < res.data.score){
-							console.log('new high ' );
-							// self.ShowHideResult(true);
-							// $('#id_text_new_score').html(res.data.score);
-							var plus_minus = '';
-							if(self._my_rank != -1){
-								var diff = self._my_rank - res.data.my_rank;
-								if(diff > 0){
-									plus_minus = '+';
-								}else if(diff < 0){
-									plus_minus = '-';
-								}
-							}
-							// $('#id_text_new_rank').html(plus_minus + res.data.my_rank);
-						}
-					}
-
-					self._my_score = res.data.score;
-					self._my_rank = res.data.rank;
-					$('#id_text_my_score').html(res.data.score);
-					$('#id_text_my_rank').html(res.data.my_rank);
-				}else{
-					$('#id_text_my_score').html('-');
-					$('#id_text_my_rank').html('-');
-				}
-			}
-		});
-	};
-
-	this.OnLogin = function(){
-		self.GetMyScoreAndRank(true/*first*/);
-		console.log('level ' + _auth_control._user_info.level);
-		
-		//FIXME 향후 user level upgrade 기능 만들고 나면
-		//아래 접근 제한 기능을 활성화 해야 함.
-		// if(_game_data.level > _auth_control._user_info.level){
-		// 	self._level_allowed_to_play = false;
-		// 	self.ShowHidePlayStopButton(false, false);
-		// 	$('#id_txt_check_level').html('You can not play <br>Game Level ' + _game_data.level);
-		// }else{
-			self._level_allowed_to_play = true;
-			self.ShowHidePlayStopButton(true, false);
-			$('#id_txt_check_level').html('');
-		// }
-	};
-
-	this.OnLogOut = function(){
-		self.OnLoginFail();
-	}
-
-	this.OnLoginFail = function(){
-		//proc guest permission
-		console.log('_game_data.level ' + _game_data.level);
-		// if(_game_data.level == 1){
-		// 	self._level_allowed_to_play = true;
-		// 	self.ShowHidePlayStopButton(true, false);
-		// 	$('#id_txt_check_level').html('');
-		// }else{
-		// 	self._level_allowed_to_play = false;
-		// 	self.ShowHidePlayStopButton(false, false);
-		// 	$('#id_txt_check_level').html('Guest user can play only<br> Game Level 1.');
-		// }
-	};
-
-	this.GetRanking = function(){
-		var data = {
-			game_id:self._game_id
-		};
-		console.log('self._game_id ' + self._game_id);
-		$.ajax({
-			url: '/beat_api/get_ranking',
-			type: 'POST',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			success: function (res) {
-				if(res.ok){
-					self.DisplayRanking(res.ranking);
-				}else{
-					alert(res.err);
-				}
-			}
-		});
-	};
-
 	this.DisplayRanking = function(ranking){
 		console.log('ranking ' + ranking.length);
 		var ele_table_ranking = $('#id_table_ranking');
@@ -545,38 +335,6 @@ function PlayGameControl(){
 
 			if(cnt == ranking.length){
 				ele_table_ranking.append($(txt));
-			}
-		});
-	};
-
-	this.SaveScore = function(){
-		if(self._is_embedded){
-			console.log('is embedded not save score. ');
-			return;
-		}
-
-		if(_auth_control._user_info == null){
-			return;
-		}
-
-		var data = {
-			game_id:self._game_id,
-			score: _game_control._score
-		};
-		$.ajax({
-			url: '/beat_api/save_score',
-			type: 'POST',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8',
-			dataType: 'json',
-			success: function (res) {
-				if(res.ok){
-					console.log('saved ');
-					self.GetRanking();
-					self.GetMyScoreAndRank(false/*not first*/);
-				}else{
-					alert(res.err);
-				}
 			}
 		});
 	};
