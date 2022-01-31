d767fa61e46365d1d372a2ed433ba9930aabaa0f
Jeonghun Lee <yi.jeonghun@gmail.com>
2022-01-31 15:01:55 +0900

    실제 서버와 동일한 url로 동작하도록 변경

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const express = require('express');
 const http = require('http');
 const app = express();
 http.createServer(app).listen(8888);
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
+app.use('/finger_dance', express.static(__dirname + '/'));
