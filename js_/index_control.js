f0b97111c8290ef1befd9d3c0784335bc96721be
Jeonghun Lee <yi.jeonghun@gmail.com>
2022-03-17 21:33:08 +0900

    played game difficulty show

--- js_/index_control.js
+++ js_/index_control.js
@@ -42,12 +42,27 @@ function IndexControl(){
 		var percent_str = '0%';
 		var score_str = '0';
 		var complete_visibility = 'complete-hide';
+		var difficulty_str = '';
 		if(game_result != null){
 			percent = game_result.progress_percent * 100;
 			percent_str = Math.round(percent, 1) + '%';
 			score_str = game_result.score;
 			if(game_result.is_complete){
 				complete_visibility = 'complete-show';
+			}
+
+			if(game_result.difficulty){
+				switch(game_result.difficulty){
+					case DIFFICULTY.EASY:
+						difficulty_str = '<span class="badge badge-primary">Easy</span>';
+						break;
+					case DIFFICULTY.NORMAL:
+						difficulty_str = '<span class="badge badge-primary">Normal</span>';
+						break;
+					case DIFFICULTY.HARD:
+						difficulty_str = '<span class="badge badge-primary">Hard</span>';
+						break;
+				}
 			}
 		}
 
@@ -65,43 +80,43 @@ function IndexControl(){
 			case GAME_TYPE.GUN_FIRE:
 				img = `<img src="../img/icon_game_type_fire.png" style="width:50px; height:auto">`;
 				break;
-			}
-
+		}
 
 		var h = `
-		<div class="col-3" onclick="${on_click}" style="margin-bottom:4em; cursor:pointer">
-			<div>
-				<image style="width:100%; height:auto" src="https://img.youtube.com/vi/${game.video_id}/0.jpg"></image>
-				<div id="id_game_complete-${game.game_id}" class="${complete_visibility}" style="position:absolute; top:10%; left:10%; width:100%; height:100%; color:red;">
-					<i class="fas fa-check" style="font-size:5em;"></i>
-				</div>
-			</div>
-			<div class="" style="margin-top: 5px">
-				<div class="d-flex">
-					<div>
-						${img}
-					</div>
-					<div class="w-100">
-						<div class="text-right" style="margin-left:5px; margin-top:5px; font-size:1.1em">${game.title}</div>
-						<div class="text-right" style="margin-left:5px; margin-top:5px; font-size:0.9em">${game.artist}</div>
+			<div class="col-3" onclick="${on_click}" style="margin-bottom:4em; cursor:pointer">
+				<div>
+					<image style="width:100%; height:auto" src="https://img.youtube.com/vi/${game.video_id}/0.jpg"></image>
+					<div id="id_game_complete-${game.game_id}" class="${complete_visibility}" style="position:absolute; top:10%; left:10%; width:100%; height:100%; color:red;">
+						<i class="fas fa-check" style="font-size:5em;"></i>
 					</div>
 				</div>
-				<div class="container" style="margin-top:5px">
-					<div class="row" style="">
-						<div class="col-6" style="padding-left:5px; font-size:0.7em">
-							Score 
-							<span id="id_game_score-${game.game_id}">${score_str}</span>
+				<div class="" style="margin-top: 5px">
+					<div class="d-flex">
+						<div>
+							${img}
 						</div>
-						<div id="id_game_percent-${game.game_id}" class="col-6 text-right" style="font-size:0.7em">
-							${percent_str}
+						<div class="w-100">
+							<div class="text-right" style="margin-left:5px; margin-top:5px; font-size:1.1em">${game.title}</div>
+							<div class="text-right" style="margin-left:5px; margin-top:5px; font-size:0.9em">${game.artist}</div>
 						</div>
 					</div>
-				</div>
-				<div id="id_game_percent_bar-${game.game_id}" 
-					style="position:absolute; bottom:0px; width:${percent}%; height:2px; background-color:blue"></div>
+					<div class="container" style="margin-top:5px">
+						<div class="row" style="">
+							<div class="col-6" style="padding-left:5px; font-size:0.7em">
+								Score 
+								<span id="id_game_score-${game.game_id}">${score_str}</span>
+							</div>
+							<div class="col-6 text-right" style="font-size:0.7em">
+								<span id="id_game_percent-${game.game_id}">${percent_str}<span>
+								<span id="id_game_difficulty-${game.game_id}">${difficulty_str}</span>
+							</div>
+						</div>
+					</div>
+					<div id="id_game_percent_bar-${game.game_id}" 
+						style="position:absolute; bottom:0px; width:${percent}%; height:2px; background-color:blue"></div>
+					</div>
 				</div>
 			</div>
-		</div>
 		`;
 		return h;
 	};
